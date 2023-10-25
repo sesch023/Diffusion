f4aa7e6661ee85f7c5e440b94c5e29713d1e6050
Sebastian Schmidt <schmidt.sebastian2@fh-swf.de>
2023-10-25 13:44:25 +0000

    Doku Latent VQGANModules

--- DiffusionModels/DiffusionModules/LatentVQGANModules.py
+++ DiffusionModels/DiffusionModules/LatentVQGANModules.py
@@ -6,9 +6,12 @@ import numpy as np
 
 from DiffusionModules.Modules import *
 
+"""
+This Module was adapted from:
+https://github.com/CompVis/taming-transformers
+"""
+
 class Encoder(nn.Module):
-    # Adapted from:
-    # https://github.com/CompVis/taming-transformers/blob/master/taming/modules/diffusionmodules/model.py#L342
     def __init__(self, 
             ch,
             in_channels,
@@ -22,6 +25,21 @@ class Encoder(nn.Module):
             emb_size=None,
             out_emb_size=1024
         ):
+        """
+        Encoder for the VQGAN model.
+
+        :param ch: Number of base channels.
+        :param in_channels: Number of input channels.
+        :param resolution: Resolution of the input.
+        :param z_channels: Number of channels in the latent space.
+        :param num_res_blocks: Number of residual blocks per resolution.
+        :param attn_resolutions: Resolutions to use attention on.
+        :param ch_mult: Channel multipliers for each resolution, defaults to (1,2,4,8)
+        :param dropout: Dropout probability, defaults to 0.0
+        :param double_z: Whether to double the number of channels in the latent space, defaults to False
+        :param emb_size: Size of the embeddings, defaults to None
+        :param out_emb_size: Output size of the linear embedding layers, defaults to 1024
+        """        
         super().__init__()
         self.ch = ch
         self.emb_size = emb_size
@@ -39,7 +57,7 @@ class Encoder(nn.Module):
                 nn.Linear(self.out_emb_size, self.out_emb_size)
             )
 
-        # downsampling
+        # Input block
         self.conv_in = nn.Conv2d(
             in_channels,
             self.ch,
@@ -48,6 +66,7 @@ class Encoder(nn.Module):
             padding=1
         )
 
+        # Construct the down blocks
         curr_res = resolution
         in_ch_mult = (1,)+tuple(ch_mult)
         self.down = nn.ModuleList()
@@ -90,7 +109,7 @@ class Encoder(nn.Module):
                 ))
             self.down.append(MultiParamSequential(*block))
 
-        # middle
+        # Output block
         self.out = MultiParamSequential(
             SelfAttentionResBlock(
                 in_channels=block_in,
@@ -107,6 +126,7 @@ class Encoder(nn.Module):
             )
         )
         self.norm_out = nn.GroupNorm(num_groups=32, num_channels=block_in, eps=1e-6, affine=True)
+        # Output conv
         self.conv_out = nn.Conv2d(
             block_in,
             2*z_channels if double_z else z_channels,
@@ -114,7 +134,6 @@ class Encoder(nn.Module):
             stride=1,
             padding=1
         )
-
         self.out_non_res = nn.Sequential(
             self.norm_out,
             nn.SiLU(),
@@ -123,6 +142,13 @@ class Encoder(nn.Module):
 
 
     def forward(self, x, emb=None):
+        """
+        Forward pass of the encoder.
+
+        :param x: Input tensor.
+        :param emb: Embedding tensor, defaults to None
+        :return: Output tensor.
+        """        
         if self.emb_size is not None:
             emb = self.emb_seq(emb) if emb is not None else torch.zeros_like(emb)
 
@@ -135,8 +161,6 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    # Adapted from:
-    # https://github.com/CompVis/taming-transformers/blob/master/taming/modules/diffusionmodules/model.py#L342
     def __init__(self, 
             ch, 
             out_channels, 
@@ -150,6 +174,21 @@ class Decoder(nn.Module):
             emb_size=None,
             out_emb_size=1024
         ):
+        """
+        Decoder for the VQGAN model.
+
+        :param ch: Number of base channels at the top.
+        :param out_channels: Number of output channels.
+        :param attn_resolutions: Resolutions to use attention on.
+        :param num_res_blocks: Number of residual blocks per resolution.
+        :param resolution: Resolution of the input.
+        :param z_channels: Number of channels in the latent space. This is the input of the decoder.
+        :param ch_mult: Channel multipliers for each resolution, defaults to (1,2,4,8)
+        :param dropout: Dropout probability, defaults to 0.0
+        :param give_pre_end: No final conv and norm, defaults to False
+        :param emb_size: Size of the embeddings, defaults to None
+        :param out_emb_size: Output size of the linear embedding layers, defaults to 1024
+        """        
         super().__init__()
         self.ch = ch
         self.emb_size = emb_size
@@ -174,6 +213,7 @@ class Decoder(nn.Module):
                 nn.Linear(self.out_emb_size, self.out_emb_size)
             )
 
+        # Input block
         self.in_conv = nn.Conv2d(
             z_channels,
             block_in,
@@ -197,7 +237,7 @@ class Decoder(nn.Module):
             )
         )
 
-        # upsampling
+        # Upsampling blocks
         self.up = nn.ModuleList()
         for i_level in reversed(range(self.num_resolutions)):
             block = []
@@ -238,6 +278,7 @@ class Decoder(nn.Module):
             self.up.append(MultiParamSequential(*block))
 
         self.norm_out = nn.GroupNorm(num_groups=32, num_channels=block_in, eps=1e-6, affine=True)
+        # Output conv
         self.conv_out = nn.Conv2d(
             block_in,
             out_channels,
@@ -253,26 +294,37 @@ class Decoder(nn.Module):
         )
 
     def forward(self, z, emb=None):
+        """
+        Forward pass of the decoder.
+
+        :param z: Input tensor of the codebook embeddings.
+        :param emb: Embedding tensor, defaults to None
+        :return: Output tensor.
+        """        
         if self.emb_size is not None:
             emb = self.emb_seq(emb) if emb is not None else torch.zeros_like(emb)
 
-        # z to block_in
+        # Input block
         h = self.in_conv(z)
         h = self.in_seq(h, emb)
 
-        # upsampling
+        # Upsampling blocks
         for block in self.up:
             h = block(h, emb)
 
-        # end
+        # Do not apply final conv and norm
         if self.give_pre_end:
             return h
 
         h = self.out_non_res(h)
         return h
 
-
 def weights_init(m):
+    """
+    Initializes the weights of convolutional and batch norm layers.
+
+    :param m: Module to initialize.
+    """    
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
@@ -281,18 +333,18 @@ def weights_init(m):
         nn.init.constant_(m.bias.data, 0)
 
 class NLayerDiscriminator(nn.Module):
-    """Defines a PatchGAN discriminator as in Pix2Pix
-        --> see https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
-    https://github.com/CompVis/taming-transformers/blob/master/taming/modules/discriminator/model.py
-    """
-
     def __init__(self, input_nc=3, ndf=64, n_layers=3):
-        """Construct a PatchGAN discriminator
-        Parameters:
-            input_nc (int)  -- the number of channels in input images
-            ndf (int)       -- the number of filters in the last (Kommentar: first??) conv layer
-            n_layers (int)  -- the number of conv layers in the discriminator
         """
+        Construct a PatchGAN discriminator.
+
+        Quellen:
+        https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
+        https://github.com/CompVis/taming-transformers/blob/master/taming/modules/discriminator/model.py
+
+        :param input_nc: Number of channels in input images, defaults to 3
+        :param ndf: Number of filters in the first conv layer, defaults to 64
+        :param n_layers: Number of conv layers in the discriminator, defaults to 3
+        """          
         super(NLayerDiscriminator, self).__init__()
 
         norm_layer = nn.BatchNorm2d
@@ -410,14 +462,6 @@ class ResidualEmbeddingConditionalDiscriminator(nn.Module):
 
 
 class VectorQuantizer(nn.Module):
-    """
-    Improved version over VectorQuantizer, can be used as a drop-in replacement. Mostly
-    avoids costly matrix multiplications and allows for post-hoc remapping of indices.
-    """
-    # NOTE: due to a bug the beta term was applied to the wrong term. for
-    # backwards compatibility we use the buggy version by default, but you can
-    # specify legacy=False to fix it.
-    # https://github.com/CompVis/taming-transformers/blob/master/taming/modules/vqvae/quantize.py#L6
     def __init__(
         self, 
         n_codebook_embeddings, 
@@ -428,6 +472,24 @@ class VectorQuantizer(nn.Module):
         sane_index_shape=False, 
         legacy=False
     ):
+        """
+        Vector quantizer for the VQGAN model. It quantizes the input to the nearest codebook embedding.
+        This was taken from:
+        https://github.com/CompVis/taming-transformers/blob/master/taming/modules/vqvae/quantize.py#L6
+
+        Original description:
+        Improved version over VectorQuantizer, can be used as a drop-in replacement. Mostly
+        avoids costly matrix multiplications and allows for post-hoc remapping of indices.
+
+        # NOTE: due to a bug the beta term was applied to the wrong term. for
+        # backwards compatibility we use the buggy version by default, but you can
+        # specify legacy=False to fix it.
+
+        :param n_codebook_embeddings: Number of codebook embeddings.
+        :param codebook_embedding_size: Size of the codebook embeddings. This equals the z_channels of the encoder and decoder.
+        :param beta: Beta parameter for the loss. This is the weight of the commitment loss.
+        :param legacy: Whether to use the legacy version of the loss, defaults to False
+        """        
         super().__init__()
         self.n_codebook_embeddings = n_codebook_embeddings
         self.codebook_embedding_size = codebook_embedding_size
@@ -453,6 +515,13 @@ class VectorQuantizer(nn.Module):
         self.sane_index_shape = sane_index_shape
 
     def remap_to_used(self, inds):
+        """
+        Remaps the indices to the used indices.
+        Since remap was not used, this was never tested.
+
+        :param inds: Indices to remap.
+        :return: Remapped indices.
+        """        
         ishape = inds.shape
         assert len(ishape)>1
         inds = inds.reshape(ishape[0],-1)
@@ -467,6 +536,13 @@ class VectorQuantizer(nn.Module):
         return new.reshape(ishape)
 
     def unmap_to_all(self, inds):
+        """
+        Unmaps the indices to the original indices.
+        Since remap was not used, this was never tested.
+
+        :param inds: Indices to unmap.
+        :return: Unmapped indices.
+        """        
         ishape = inds.shape
         assert len(ishape)>1
         inds = inds.reshape(ishape[0],-1)
@@ -477,6 +553,12 @@ class VectorQuantizer(nn.Module):
         return back.reshape(ishape)
 
     def forward(self, z, temp=None, rescale_logits=False, return_logits=False):
+        """
+        Forward pass of the vector quantizer. Also calculates the quantization loss.
+
+        :param z: Non quantized input tensor.
+        :return: _description_
+        """        
         assert temp is None or temp==1.0, "Only for interface compatible with Gumbel"
         assert rescale_logits==False, "Only for interface compatible with Gumbel"
         assert return_logits==False, "Only for interface compatible with Gumbel"
@@ -520,6 +602,13 @@ class VectorQuantizer(nn.Module):
         return z_q, loss, (perplexity, min_encodings, min_encoding_indices)
 
     def get_codebook_entry(self, indices, shape):
+        """
+        Gets the codebook entry for the given indices.
+
+        :param indices: Indices to get the codebook entry for.
+        :param shape: Shape of the output if not None.
+        :return: Codebook entry.
+        """        
         # shape specifying (batch, height, width, channel)
         if self.remap is not None:
             indices = indices.reshape(shape[0],-1) # add batch axis
